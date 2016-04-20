118ab44a8b6844de46c8320bc2d862fd2a6cb86d
Carol (Nichols || Goulding) <carol.nichols@gmail.com>
2016-04-20 19:27:24 -0400

    Redirect to review URL, less annoying than back to github

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,7 @@ class SessionsController < ApplicationController
     if user
       session[:user_id] = user.id
       flash[:notice] = "Signed In"
-      redirect_to :back
+      redirect_to "/review/rbr2016"
     else
       flash[:notice] = "Not Authorized"
       redirect_to root_url
