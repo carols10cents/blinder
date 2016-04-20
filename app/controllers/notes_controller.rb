e4ea05dd2777c5497d9b977734a6c800f195687b
Carol (Nichols || Goulding) <carol.nichols@gmail.com>
2016-04-20 11:27:57 -0400

    Fetch and save notes by current user
    An evil reviewer (you don't have any of those, do you? Let's just be
    sure) could create/edit anyone's notes without this.

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -6,12 +6,12 @@ class NotesController < ApplicationController
   before_filter :has_access?
 
   def create
-    note = Note.new note_ajax_params[:note]
+    note = current_user.notes.build note_ajax_params[:note]
     render json: { success: note.save, message: note.save_message }
   end
 
   def update
-    note = Note.find params[:id]
+    note = current_user.notes.find params[:id]
     note.update note_ajax_params[:note]
     render json: { success: note.save, message: note.save_message }
   end
@@ -19,7 +19,7 @@ class NotesController < ApplicationController
   protected
 
   def note_ajax_params
-    params.permit(note: [:user_id, :proposal_id, :content])
+    params.permit(note: [:proposal_id, :content])
   end
 
 end
