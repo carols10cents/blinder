880174c176dbac2779d6c390d3658aa988bd3a9b
Carol (Nichols || Goulding) <carol.nichols@gmail.com>
2018-08-13 17:07:31 -0400

    Revert "Revert "Make score uneditable""
    This reverts commit 12c8e4c0cb8b63a9452c7f3e6cbe974e967d3ed8.

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -19,7 +19,7 @@ class NotesController < ApplicationController
   protected
 
   def note_ajax_params
-    params.permit(note: [:proposal_id, :content, :score])
+    params.permit(note: [:proposal_id, :content])
   end
 
 end
